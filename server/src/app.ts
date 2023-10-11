15dabc29c79dddf54f3640491c9dcef88ab5b002
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-10-11 15:29:48 -0300

    feat: add many registers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,32 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import fastifyPassport from '@fastify/passport'
+import fastifySecureSession from '@fastify/secure-session'
+
+import { loginRoutes } from './routes/login'
+import { usersRoutes } from './routes/users'
+import { mealsRoutes } from './routes/meals'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(cookie)
+
+app.register(fastifySecureSession, {
+  secret: env.SECRET,
+  salt: 'mq9hDxBVDbspDR6n',
+  cookieName: 'sessionId',
+  cookie: {
+    path: '/',
+  },
+})
+app.register(fastifyPassport.initialize())
+app.register(fastifyPassport.secureSession())
+
+app.register(loginRoutes)
+app.register(usersRoutes, {
+  prefix: 'users',
+})
+app.register(mealsRoutes, {
+  prefix: 'meals',
+})
