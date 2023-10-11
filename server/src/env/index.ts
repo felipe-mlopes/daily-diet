1488fde446d0e7e520ad1abaec84be7d51838efa
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-10-11 15:27:13 -0300

    feat: add google client id, google secret & secret

--- server/src/env/index.ts
+++ server/src/env/index.ts
@@ -11,6 +11,9 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 })
 
